599ff6cf33573a306a491c0453aa6dcf05c435fb
ajmarrocco <ajmarrocco@gmail.com>
2022-01-09 21:00:44 -0500

    adds message to highscore html

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,9 @@ var countEl = document.querySelector('#count');
 var buttonEl = document.querySelector('#btn');
 var beginningEl = document.querySelector('#beginning');
 var instructionsEl = document.querySelector('#instructions');
+var clearButtonEl = document.querySelector("#clear-btn");
 
+//set array of objects
 var questions = [
     {
         question:"Commonly used data types DO Not Include:",
@@ -52,15 +54,14 @@ var questions = [
     }
 ];
 
-console.log(questions[0].question);
-
 //tells user the answer is right or wrong
 var confirmEl = document.createElement("div");
 //divs for questions
 var divEl = document.createElement("div");
 //create ordered list questions
 var questionEl = document.createElement("ol");
-confirmEl.className = "cnfm"
+//Class names for styling in CSS
+confirmEl.className = "cnfm";
 divEl.className = "divdiv";
 questionEl.className = "quest";
 //create list item answers for questions
@@ -73,17 +74,16 @@ var buttonOneEl = document.createElement("button");
 var buttonTwoEl  = document.createElement("button");
 var buttonThreeEl = document.createElement("button");
 var buttonFourEl = document.createElement("button");
-
+//Class names for styling in CSS
 answerOneEl.className = "li-style";
 answerTwoEl.className = "li-style";
 answerThreeEl.className = "li-style";
 answerFourEl.className = "li-style";
-
+//Class names for styling in CSS
 buttonOneEl.className = "new-btn";
 buttonTwoEl.className = "new-btn";
 buttonThreeEl.className = "new-btn";
 buttonFourEl.className = "new-btn";
-
 //Creates Elements for submitScore function
 var finalMessageEl = document.createElement("div");
 var finalEl = document.createElement("h1");
@@ -93,7 +93,7 @@ var initialsLabelEl = document.createElement("label");
 var initialsInputEl = document.createElement("input");
 var highScorePageEl = document.createElement("a");
 var submitScoreEl = document.createElement("button");
-
+//Classes Elements for submitScore function
 finalMessageEl.classname = "final-msg";
 finalEl.className = "all-done";
 scoreMessageEl.className = "score-msg";
@@ -102,107 +102,41 @@ initialsLabelEl.className = "init-label";
 initialsInputEl.className = "init-input";
 highScorePageEl.setAttribute("href", "./highscore.html");
 submitScoreEl.className = "submit-btn";
-
-//subtracts 15
+//subtracts 10
 var penalty = function(){ 
     this.count -= countSubtract;
-    // console.log(count);
 }
+//TO DO: displays score to user
+var showScore = function(){
+    var bestScore = savingScore();
+    var scoreText = document.querySelector("#score");
 
-var selectQuestion = function(){
-
-    for (let i = 0; i < questions.length; i++){
-        main.appendChild(divEl);
-        divEl.appendChild(questionEl);
-
-        // debugger;
-        
-        var someFunc = function(index){
-        questionEl.textContent = questions[i].question;
-
-        questionEl.appendChild(answerOneEl);
-        answerOneEl.appendChild(buttonOneEl);      
-        questionEl.appendChild(answerTwoEl);
-        answerTwoEl.appendChild(buttonTwoEl);
-        questionEl.appendChild(answerThreeEl);
-        answerThreeEl.appendChild(buttonThreeEl);      
-        questionEl.appendChild(answerFourEl);
-        answerFourEl.appendChild(buttonFourEl);
-
-        buttonOneEl.textContent = questions[i].answer1;
-        buttonTwoEl.textContent = questions[i].answer2;
-        buttonThreeEl.textContent = questions[i].answer3;
-        buttonFourEl.textContent = questions[i].answer4;
-        console.log(questions[i].answer4);
-        // debugger;
-        buttonOneEl.addEventListener("click", function(){        
-            if (questions[i].answer1 === questions[i].correct){
-                main.appendChild(confirmEl);
-                confirmEl.textContent = "Correct!";
-                penalty();
-            } else{
-                main.appendChild(confirmEl);
-                confirmEl.textContent = "Wrong!";
-                penalty();
-            }
-        });
-        buttonTwoEl.addEventListener("click", function(){        
-            if (questions[i].answer2 === questions[i].correct){
-                main.appendChild(confirmEl);
-                confirmEl.textContent = "Correct!";
-                penalty();
-            } else{
-                main.appendChild(confirmEl);
-                confirmEl.textContent = "Wrong!";
-                penalty();
-            }
-        });
-        buttonThreeEl.addEventListener("click", function(){        
-            if (questions[i].answer3 === questions[i].correct){
-                main.appendChild(confirmEl);
-                confirmEl.textContent = "Correct!";
-                penalty();
-            } else{
-                main.appendChild(confirmEl);
-                confirmEl.textContent = "Wrong!";
-                penalty();
-            }
-        });
-        buttonFourEl.addEventListener("click", function(){        
-            if (questions[i].answer4 === questions[i].correct){
-                main.appendChild(confirmEl);
-                confirmEl.textContent = "Correct!";
-                penalty();
-            } else{
-                main.appendChild(confirmEl);
-                confirmEl.textContent = "Wrong!";
-                penalty();
-            }
-        });
-    }
-    someFunc(i);
-    }
+    scoreText.textContent = bestScore;   
 }
-
+//saves count to storage
 var savingScore = function(){
     submitScoreEl.addEventListener("click", function(){
     localStorage.setItem(initialsInputEl.value, JSON.stringify(count));
     });
 
-}
+    var savedScore = localStorage.setItem(initialsInputEl.value, JSON.stringify(count));
 
-var loadScore = function(){
-    var savedScore = localStorage.getItem(initialsInputEl.value);
-
-    if(!savingScore){
-        return false;
+    if(savedScore === null){
+        savedScore = 0;
     }
 
-    savedScore = JSON.parse(savedScore);
+    if(this.count > count){
+        localStorage.setItem(initialsInputEl.value, JSON.stringify(count));
+    }
+
+    console.log(savedScore);
+
+    return savedScore;
 }
 
-// saves the score
+// submits the score
 var submitScore = function(){
+    //creates elements to save score
     main.appendChild(finalMessageEl);
     finalMessageEl.appendChild(finalEl);
     finalEl.textContent = "All done!";
@@ -215,11 +149,94 @@ var submitScore = function(){
     scoreContainerEl.appendChild(highScorePageEl);
     submitScoreEl.textContent = "Submit";
     highScorePageEl.appendChild(submitScoreEl);
-
+    //runs saves score
     savingScore();
-
-    loadScore();
+    // runs display score
+    showScore();
 }
+
+//TO DO: Selects question
+var selectQuestion = function(){
+    //loops through object array
+    for (let i = 0; i < questions.length; i++){
+        main.appendChild(divEl);
+        divEl.appendChild(questionEl);
+        //appends the questions, buttons and answers
+        var appendsQuestion = function(index){
+
+            questionEl.textContent = questions[index].question;
+
+            questionEl.appendChild(answerOneEl);
+            answerOneEl.appendChild(buttonOneEl);      
+            questionEl.appendChild(answerTwoEl);
+            answerTwoEl.appendChild(buttonTwoEl);
+            questionEl.appendChild(answerThreeEl);
+            answerThreeEl.appendChild(buttonThreeEl);      
+            questionEl.appendChild(answerFourEl);
+            answerFourEl.appendChild(buttonFourEl);
+
+            buttonOneEl.textContent = questions[index].answer1;
+            buttonTwoEl.textContent = questions[index].answer2;
+            buttonThreeEl.textContent = questions[index].answer3;
+            buttonFourEl.textContent = questions[index].answer4;
+            console.log(questions[index].answer4);
+            //runs if button 1 is clicked
+            buttonOneEl.addEventListener("click", function(){        
+                if (questions[index].answer1 === questions[index].correct){
+                    main.appendChild(confirmEl);
+                    confirmEl.textContent = "Correct!";
+                    penalty();
+                } else{
+                    main.appendChild(confirmEl);
+                    confirmEl.textContent = "Wrong!";
+                    penalty();
+                }
+            });
+            //runs if button 4 is clicked
+            buttonTwoEl.addEventListener("click", function(){        
+                if (questions[index].answer2 === questions[index].correct){
+                    main.appendChild(confirmEl);
+                    confirmEl.textContent = "Correct!";
+                    penalty();
+                } else{
+                    main.appendChild(confirmEl);
+                    confirmEl.textContent = "Wrong!";
+                    penalty();
+                }
+            });
+            //runs if button 3 is clicked
+            buttonThreeEl.addEventListener("click", function(){        
+                if (questions[index].answer3 === questions[index].correct){
+                    main.appendChild(confirmEl);
+                    confirmEl.textContent = "Correct!";
+                    penalty();
+                } else{
+                    main.appendChild(confirmEl);
+                    confirmEl.textContent = "Wrong!";
+                    penalty();
+                }
+            });
+            //runs if button 4 is clicked
+            buttonFourEl.addEventListener("click", function(){        
+                if (questions[index].answer4 === questions[index].correct){
+                    main.appendChild(confirmEl);
+                    confirmEl.textContent = "Correct!";
+                    penalty();
+                } else{
+                    main.appendChild(confirmEl);
+                    confirmEl.textContent = "Wrong!";
+                    penalty();
+                }
+            });
+        }   
+    appendsQuestion(i);
+    }   
+    //TO DO: runs submit score after fifth question
+    if (questions.length === 4){
+        submitScore();
+    }    
+}
+
 
 //creates the timer function and removes beginning title and instructions
 var setTimer = function(){
@@ -233,22 +250,26 @@ var setTimer = function(){
     selectQuestion();
     //creates the countdown function which decreases count by one and reassigns count to that value
     var countdown = function(){
-        // console.log(count);
         countEl.textContent = count;
         count--;
-        //stops the timer when it reaches zero and shows alert message
+        //stops the timer when it reaches zero or below zero and shows save message
         if(count <= 0){
+            count = 0;
+            countEl.textContent = 0;
             clearInterval(myTimer);     
             submitScore();
         } 
     }
     //runs the countdown function every 1000 ms or 1s
-    var myTimer = setInterval(countdown, 1000);
+    var myTimer = setInterval(countdown, 1000);  
 }
 
-//creates function that decreases count by one
-
-// // Set the timer when you click the "Start Quiz" button
+// Set the timer when you click the "Start Quiz" button
 buttonEl.addEventListener("click", function(){
     setTimer();
+});
+
+// TO DO: clears storage
+clearButtonEl.addEventListener("click", function(){
+    localStorage.clear();
 });
